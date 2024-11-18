31109de126cdcb927b4eb3f54c1e148792c15736
ahmedkamal <ahmed.kamal@webstdy.com>
2024-11-18 11:51:33 +0200

    change logo and site name

--- resources/views/partials/dashboard/head.blade.php
+++ resources/views/partials/dashboard/head.blade.php
@@ -25,7 +25,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
-    <link rel="shortcut icon" type="image/gif" sizes="16x16" href="{{ asset('fav.svg') }}" />
+    <link rel="shortcut icon" type="image/gif" sizes="16x16" href="{{  asset(getImagePathFromDirectory(settings()->getSettings('favicon'), 'Settings')) }}" />
 
     <!--begin::Fonts-->
     <link rel="preconnect" href="https://fonts.googleapis.com">

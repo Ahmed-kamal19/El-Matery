657a71504033e7dbedfdb1f1b0fe6525a5c9bffb
ahmed kamal <eng.ahmedkamal357@gmail.com>
2025-02-09 16:01:42 +0200

    change service resource

--- app/Http/Resources/ServiceResource.php
+++ app/Http/Resources/ServiceResource.php
@@ -21,8 +21,8 @@ class ServiceResource extends JsonResource
         return [
             'id'=>$this->id,
             'name'=>$this->name,
-            'price_before_discount'=>$this->price,
-            'price'=>$this->discount_price ??0,
+            'price_before_discount'=>$this->discount_price ? ceil($this->price):0,
+            'price'=>$this->discount_price ??$this->price,
             'price_after_tax' => $this->getPriceAfterVatAttribute() == $this->price ? 0:$this->getPriceAfterVatAttribute(),
             'image'=>getImagePathFromDirectory($this->image,'Services'),
             'description'=>$this->description,

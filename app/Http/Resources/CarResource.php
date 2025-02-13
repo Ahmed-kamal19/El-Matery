db19dcb69009ee5f64905056e98e2116b2318b97
ahmed kamal <eng.ahmedkamal357@gmail.com>
2025-02-13 17:19:42 +0200

    change filter api and car response

--- app/Http/Resources/CarResource.php
+++ app/Http/Resources/CarResource.php
@@ -25,9 +25,9 @@ class CarResource extends JsonResource
         // "price"=> $this->price,
         'show_status'=>$show_status,
         'price_field_status'=>__($price_field_status) === __('others') ? $this->other_description:__($price_field_status),
-        'price'=>$price_field_status === PriceFieldStatus::show_details->name ?number_format($this->discount_price):0,
-        'price_before_discount'=>$price_field_status === PriceFieldStatus::show_details->name && $this->have_discount?number_format($this->price) :0,
-        'price_after_vat' =>$price_field_status === PriceFieldStatus::show_details->name ? ($this->price_after_vat ==$this->price ? 0 :number_format($this->price_after_vat) ):0,
+        'price'=>$price_field_status === PriceFieldStatus::show_details->name ?(int)($this->discount_price):0,
+        'price_before_discount'=>$price_field_status === PriceFieldStatus::show_details->name && $this->have_discount?(int)($this->price) :0,
+        'price_after_vat' =>$price_field_status === PriceFieldStatus::show_details->name ? ($this->price_after_vat ==$this->price ? 0 :(int)($this->price_after_vat) ):0,
        
         // "price_after_vat"=>$this->price_after_vat ==$this->price ? 0 :$this->price_after_vat ,
         "fuel_type"=>__($this->fuel_type),
